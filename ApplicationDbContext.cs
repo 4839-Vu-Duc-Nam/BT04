2df01dd914784592bfdc5a794059d22d6d1edb0a
4839-Vu-Duc-Nam <yolodinam.ok@gmail.com>
2026-02-06 22:10:31 +0700

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BT04.Models;
+
+namespace BT04.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ TodoController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BT04.Data;
+using BT04.Models;
+
+namespace BT04.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // 1. Danh sách
+        public IActionResult Index()
+        {
+            return View(_context.Todos.ToList());
+        }
+
+        // 2. Thêm mới (GET)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // 3. Thêm mới (POST)
+        [HttpPost]
+        public IActionResult Create(Todo todo)
+        {
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // 4. Sửa (GET)
+        public IActionResult Edit(int id)
+        {
+            return View(_context.Todos.Find(id));
+        }
+
+        // 5. Sửa (POST)
+        [HttpPost]
+        public IActionResult Edit(Todo todo)
+        {
+            _context.Todos.Update(todo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // 6. Chi tiết
+        public IActionResult Details(int id)
+        {
+            return View(_context.Todos.Find(id));
+        }
+
+        // 7. Xóa
+        public IActionResult Delete(int id)
+        {
+            var todo = _context.Todos.Find(id);
+            _context.Todos.Remove(todo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
